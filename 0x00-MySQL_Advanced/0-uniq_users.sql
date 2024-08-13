7e646f2e6dae31c40c56cd8d4849d48ec8cc9665
essaiydy <imaneessaiydy2@gmail.com>
2024-08-12 19:08:38 -0700

    adv sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- If table exists, script will not fail, can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- Script should execute on any database
+
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
